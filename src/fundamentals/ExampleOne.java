47006a9375354102598de3ca97dc492a0e4332af
Ram <mrdram28@gmail.com>
2016-04-10 15:52:59 +0530

    Example One First Commit

---
+++ src/fundamentals/ExampleOne.java
@@ -0,0 +1,12 @@
+package fundamentals;
+
+public class ExampleOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is my first github java program");
+
+	}
+
+}
